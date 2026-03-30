964553ffb2b6332eb1d06052257928832d7f6001
sadeeq shaik <sadeeqshaik@sadeeqs-MacBook-Air.local>
2026-03-30 20:24:10 +0530

    authenctiation completed.

--- main.py
+++ main.py
@@ -96,6 +96,24 @@ async def user_posts_page(request: Request, user_id: int, db: Annotated[AsyncSes
         {"posts": posts, "user": user, "title": f"{user.username}'s Posts"},
     )
 
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
+
+
+
 # --------------------------------------- Template Loading -----------------------------------------#
 
 
